e0ec236e30cc426cb8ef824d0e2658ff1c57049a
William Sedlacek <wsedlacekc@gmail.com>
2020-07-24 10:19:30 -0700

    fix: RouterLink
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- apps/apartments/src/app/shared/shared.module.ts
+++ apps/apartments/src/app/shared/shared.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
 import { QuicklinkModule } from 'ngx-quicklink';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 @NgModule({
-  exports: [CommonModule, QuicklinkModule, MatButtonModule, MatCardModule],
+  exports: [
+    CommonModule,
+    RouterModule,
+    QuicklinkModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
 })
 export class SharedModule {}
